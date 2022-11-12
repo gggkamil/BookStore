944bb629aa43ca84feb3934d27f8a699f788b063
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-12 21:15:51 +0100

    Genre commit

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre{ get; set; }
 
         public ICollection<BookCategory> BookCategories { get; set; }
     }
