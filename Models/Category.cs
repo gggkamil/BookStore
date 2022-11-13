e08d89f6b208022bade93fb20b47186aa5189295
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-13 19:30:04 +0100

    Genreminus

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Genre{ get; set; }
 
         public ICollection<BookCategory> BookCategories { get; set; }
     }
